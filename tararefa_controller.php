3ff6c62660dc2df12c42b22df267326da1f254df
Carl0s-Anderson <carlosanderson30030@gmail.com>
2026-03-31 21:27:34 -0300

    lista e atualiza registros

--- tararefa_controller.php
+++ tararefa_controller.php
@@ -2,14 +2,29 @@
 require "tarefa_moidel.php";
 require "tarefa_service.php";
 require "conexao.php";
+$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
+if('$acao' == 'inserir') {
+    $tarefa = new tarefa();
+    $tarefa->__set('tarefa', $_POST['tarefa']);
 
+    $conexao = new conexao();
+    $tarefaService = new Tarefa_service($conexao, $tarefa);
+    $tarefaService->criar();
+    header('Location: nova_tarefa.php?sucesso=1');
+} elseif ($acao == 'recuperar') {
+    $tarefa = new tarefa();
+    $conexao = new conexao();
+    $tarefaService = new Tarefa_service($conexao, $tarefa);
+    $tarefas = $tarefaService->recuperar();
+} elseif ($acao == 'atualizar') {
+   $tarefa = new tarefa();
+    $tarefa->__set('id', $_POST['id']);
+    $tarefa->__set('tarefa', $_POST['tarefa']);
 
-$tarefa = new tarefa();
-$tarefa->__set('tarefa', $_POST['tarefa']);
-
-$conexao = new conexao();
-$tarefaService = new Tarefa_service($conexao, $tarefa);
-$tarefaService->criar();
-
-header('Location: nova_tarefa.php?sucesso=1');
+    $conexao = new conexao();
+  $tarefaService = new Tarefa_service($conexao, $tarefa);
+    if ($tarefaService->atualizar()) {
+        header('Location: index.php?sucesso=1');
+    }
+} 
--- tarefa_service.php
+++ tarefa_service.php
@@ -12,7 +12,8 @@ class Tarefa_service
     $this->tarefa = $tarefa;
   }
 
-  public function criar() {
+  public function criar()
+  {
 
     $query = 'insert into tb_tarefas(tarefa) values(:tarefa)';
     $stmt = $this->conexao->prepare($query);
@@ -20,14 +21,26 @@ class Tarefa_service
     $stmt->execute();
   } //criar
   public function recuperar()
-  { // lera
+  {
 
-
+    $query = '
+     select 
+        t.id, s.status, t.tarefa 
+      from 
+        tb_tarefas as t 
+      left join 
+        tb_status as s on (t.id_status = s.id)';
+    $stmt = $this->conexao->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_OBJ);
   }
-  public function atualizar()
-  { //atualizar
-
-
+  public function atualizar( )
+  { 
+   $query = 'update tb_tarefas set tarefa = :tarefa where id = :id';
+    $stmt = $this->conexao->prepare($query);
+    $stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+    $stmt->bindValue(':id', $this->tarefa->__get('id'));
+    return $stmt->execute();
   }
   public function deletar()
   { //deletar
